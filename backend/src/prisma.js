b2230ec96a122d5131fb1c54b8f96cb2c9e46381
Suraj Kumar Jaiswal <sj029033@gmail.com>
2025-09-14 19:35:31 +0530

    Update prisma.js

--- backend/src/prisma.js
+++ backend/src/prisma.js
@@ -1,18 +1,11 @@
 const { PrismaClient } = require('@prisma/client');
 
-let prisma;
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient({
-    log: ['error'], // Optional: reduce noise
-  });
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient({
-      log: ['query', 'error', 'info', 'warn'], // Helpful for debugging locally
-    });
-  }
-  prisma = global.prisma;
-}
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 module.exports = prisma;
